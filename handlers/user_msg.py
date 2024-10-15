4854ca6bfdd560c09334fd5ca3fbe0d950bc0552
Tastes_like_joy <ru00012r@gmail.com>
2024-10-15 20:23:09 +0300

    four commit

--- handlers/user_msg.py
+++ handlers/user_msg.py
@@ -10,17 +10,3 @@ router = Router()
 @router.message(CommandStart())
 async def start_command(message:Message):
     await message.answer(f'Привет, {message.from_user.username}!', reply_markup=reply.main_kb)
-
-
-
-@router.message(Command(commands=['timer', 'time']))
-async def timer(message: Message, command: CommandObject):
-    a, b = [int(n) for n in command.args.split('-')]
-    r = reply.randint(a, b)
-
-    await message.reply(f'Random number: {r}')
-
-
-# @router.message(Command('test'))
-# async def test(message: Message, bot: Bot):
-#     await bot.send_message(message.chat.id, 'test')
